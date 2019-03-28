ef94b3304b0e43fae8018bfae941a67d48ef449d
Your Nameshudengnyc <shuliang.deng@columbia.edu>
2019-03-28 16:10:42 +0000

    data cleaning file

---
+++ data_cleaning.R
@@ -0,0 +1,19 @@
+# Import data 
+library(tidyverse)
+raw_df_train = read_csv("data/train.csv")
+raw_df_stores = read_csv("data/stores.csv")
+raw_df_ftrs = read_csv("data/features.csv")
+
+# Merge data
+df_merged = merge(merge(raw_df_train,raw_df_ftrs),raw_df_stores)
+
+## change markdown to either have or don't have markdown 
+df_merged$MarkDown1 = ifelse(is.na(df_merged$MarkDown1),0,1)
+df_merged$MarkDown2 = ifelse(is.na(df_merged$MarkDown2),0,1)
+df_merged$MarkDown3 = ifelse(is.na(df_merged$MarkDown3),0,1)
+df_merged$MarkDown4 = ifelse(is.na(df_merged$MarkDown4),0,1)
+df_merged$MarkDown5 = ifelse(is.na(df_merged$MarkDown5),0,1)
+head(df_merged)
+table(df_merged$MarkDown1)
+
+str(df_merged$Date)
